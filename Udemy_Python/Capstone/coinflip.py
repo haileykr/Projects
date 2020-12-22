738fe6a9cb37b3ff9faf7cd5041355bf5efc6248
haileykr <41278804+haileykr@users.noreply.github.com>
2020-12-22 22:17:31 +0900

    Create coinflip.py

---
+++ Udemy_Python/Capstone/coinflip.py
@@ -0,0 +1,31 @@
+'''
+Coin Flip
+Flipping a single coin however many times the user decides.
+The code should record the outcomes and count the number of tails and heads.
+12/22/20
+'''
+
+from random import randint
+
+def coinFlip(numOfFlips):
+    coinSides = {0: "head", 1:"tail"}
+    flips = []
+    numHeads = numTails = 0
+    for num in range(0,numOfFlips):
+        flip = randint(0,1)
+        flips.append(coinSides[flip])
+        if flip == 0:
+            numHeads += 1
+        else:
+            numTails += 1
+    return f"Your flip results are: {flips}, with {numHeads} heads and {numTails} tails"
+
+def main():
+    numOfFlips = int(input("How many times do you wanna flip a coin?: "))
+
+    print(coinFlip(numOfFlips))
+
+if __name__ == '__main__':
+
+    main()
+
