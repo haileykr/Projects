df96c76d132a0f33b713c7112b6330d6b217896d
haileykr <41278804+haileykr@users.noreply.github.com>
2021-03-05 13:34:57 +0900

    modified for deployment

--- CitiesAndBuildings/app.js
+++ CitiesAndBuildings/app.js
@@ -30,7 +30,7 @@ const buildingRoutes = require('./routes/buildings');
 const MongoDBStore =require('connect-mongo').default;
 // const { v4: uuid } = require('uuid'); //For generating ID's
 
-const dbUrl = "mongodb://localhost:27017/citiesandbuildings"
+const dbUrl =process.env.DB_URL || "mongodb://localhost:27017/citiesandbuildings"
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
